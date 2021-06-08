4e5f8a29ded165deeb3dad2b65134e5be529d5db
Tibor Rozsa <tibor.rozsa@tutanota.com>
2021-06-08 13:35:09 +0200

    Change run model script

--- LSV1M/run_model_slurm.py
+++ LSV1M/run_model_slurm.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
-import sys 
-from mozaik.meta_workflow.parameter_search import CombinationParameterSearch,SlurmSequentialBackend
+import sys
+from mozaik.meta_workflow.parameter_search import (
+    CombinationParameterSearch,
+    SlurmSequentialBackend,
+)
 import numpy
 import time
-slurm_options = ['-J model']
 
-if True:
-    CombinationParameterSearch(SlurmSequentialBackend(
-            num_threads=16,
-            num_mpi=1,
-            path_to_mozaik_env='/home/rozsa/virt_env/mozaik/bin/activate',
-            slurm_options=slurm_options),{
-                                         'trial': [1]
-                                         }).run_parameter_search()
+slurm_options = ["-J 1130sparse_noise", "-w w3", "--mem=64gb", "--hint=nomultithread"]
+
+CombinationParameterSearch(
+    SlurmSequentialBackend(
+        num_threads=16,
+        num_mpi=1,
+        path_to_mozaik_env="/home/rozsa/virt_env/mozaik/bin/activate",
+        slurm_options=slurm_options,
+    ),
+    {"trial": [1]},
+).run_parameter_search()
